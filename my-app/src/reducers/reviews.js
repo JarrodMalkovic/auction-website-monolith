6be934b4846fb0f4a7d7d7567e1601bdf9a7ec36
jarrod-malkovic <jarrodmalkovic@outlook.com>
2020-03-25 03:57:13 +0800

    Added review actions

--- my-app/src/reducers/reviews.js
+++ my-app/src/reducers/reviews.js
@@ -1,12 +1,14 @@
 import {
   GET_REVIEWS,
   CLEAR_REVIEWS,
+  CREATE_REVIEW,
+  EDIT_REVIEW,
   DELETE_REVIEW,
   MARK_REVIEW_HELPFUL
 } from '../actions/types';
 
 const initialState = {
-  data: null,
+  data: [],
   loading: true
 };
 
@@ -24,14 +26,28 @@ export default function(state = initialState, action) {
     case GET_REVIEWS:
       return {
         ...state,
-        data: payload,
+        data: state.data.concat(payload),
         loading: false
       };
-
+    case CREATE_REVIEW:
+      return {
+        ...state,
+        data: state.data.concat(payload),
+        loading: false
+      };
+    case EDIT_REVIEW:
+      console.log(payload);
+      return {
+        ...state,
+        data: state.data.map(review =>
+          review._id == payload._id ? payload : review
+        ),
+        loading: false
+      };
     case CLEAR_REVIEWS:
       return {
         ...state,
-        data: null,
+        data: [],
         loading: true
       };
     case DELETE_REVIEW:
