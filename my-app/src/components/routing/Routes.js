6e716871f11dd13653fff750487a4d1f4f8a82b0
jarrod-malkovic <jarrodmalkovic@outlook.com>
2020-03-25 03:57:43 +0800

    Removed edit review route

--- my-app/src/components/routing/Routes.js
+++ my-app/src/components/routing/Routes.js
@@ -1,23 +1,21 @@
 import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from '../auth/LoginPage';
-import Register from '../auth/RegisterPage';
-import Listings from '../Listings/Listings';
-import Listing from '../Listing/Listing';
-import ListingForm from '../Forms/ListingForm';
+import Login from '../Auth/LoginPage';
+import Register from '../Auth/RegisterPage';
+import ListingsPage from '../Listings/ListingsPage';
+import ListingPage from '../Listing/ListingPage';
 import EditListingPage from '../Listing/EditListingPage';
 import CreateListingPage from '../Listing/CreateListingPage';
 import { loadUser } from '../../actions/auth';
 import { connect } from 'react-redux';
-import PrivateRoute from '../routing/PrivateRoute';
+import PrivateRoute from '../Routing/PrivateRoute';
 import ProfilePage from '../Profile/ProfilePage';
 import Dashboard from '../Dashboard/Dashboard';
-import CreateReviewPage from '../Reviews/CreateReviewPage';
 import EditProfilePage from '../Profile/EditProfilePage';
+import HomePage from '../Homepage/HomePage';
 import YourListingsPage from '../Dashboard/YourListingsPage';
 import BiddingHistoryPage from '../Dashboard/BiddingHistoryPage';
 import YourReviewsPage from '../Dashboard/YourReviewsPage';
-import EditReviewPage from '../Reviews/EditReviewPage';
 import PageNotFound from '../Layout/PageNotFound';
 
 const Routes = ({ loadUser, dispatch }) => {
@@ -27,10 +25,11 @@ const Routes = ({ loadUser, dispatch }) => {
   return (
     <div>
       <Switch>
+        <Route exact path='/home' component={HomePage} />\
         <Route exact path='/login' component={Login} />\
         <Route exact path='/register' component={Register} />
-        <Route exact path='/listings' component={Listings} />
-        <Route exact path='/listings/:slug' component={Listing} />
+        <Route exact path='/listings' component={ListingsPage} />
+        <Route exact path='/listings/:slug' component={ListingPage} />
         <PrivateRoute exact path='/create' component={CreateListingPage} />
         <PrivateRoute
           exact
@@ -38,13 +37,7 @@ const Routes = ({ loadUser, dispatch }) => {
           component={EditListingPage}
         />
         <Route exact path='/profile/:id' component={ProfilePage} />
-        <Route exact path='/profile/:id/review' component={CreateReviewPage} />
         <PrivateRoute exact path='/dashboard' component={Dashboard} />
-        <PrivateRoute
-          exact
-          path='/reviews/:id/edit'
-          component={EditReviewPage}
-        />
         <PrivateRoute
           exact
           path='/dashboard/edit'
