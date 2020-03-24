bfa37c1ebb05725b74894a8fdc1976e1a17391c4
jarrod-malkovic <jarrodmalkovic@outlook.com>
2020-03-25 03:33:54 +0800

    Created different db for enviroments

--- config/db.js
+++ config/db.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const dotenv = require('dotenv').config({ path: './config.env' });
 
 const connectDB = async () => {
   try {
+    if (process.env.NODE_ENV === 'test') {
+      var dbName = 'test';
+    } else {
+      var dbName = 'Production';
+    }
+
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      dbName
     });
 
-    console.log('MongoDB Connected...');
+    console.log(`MongoDB Connected to ${dbName} DB...`);
   } catch (err) {
     console.log(err.message);
-    //Exit process with failure
     process.exit(1);
   }
 };
